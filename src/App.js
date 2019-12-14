6ffafd4596f94966e52400e15652a55a74c2a6fa
abelh <abelh@localhost.localdomain>
2019-12-14 14:58:16 -0600

    Added login with social networks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import firebase from 'firebase'
+import {
+  BrowserRouter as Router, // Enrutador
+  Switch, // navegación entre rutas
+  Route, // ruta
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// css
+import 'bulma/css/bulma.css'
+import './App.css'
+
+// Pages
+import Login from './pages/login'
+import Home from './pages/home'
+
+// Setup firebase
+let firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "atom-insta10.firebaseapp.com",
+  databaseURL: "https://atom-insta10.firebaseio.com",
+  projectId: "atom-insta10",
+  storageBucket: "atom-insta10.appspot.com",
+  messagingSenderId: "207336179354",
+  appId: "1:207336179354:web:90259c37b5755e88ab8123",
+  measurementId: "G-844694ZWG8"
+}
+firebase.initializeApp(firebaseConfig)
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (<div className="container">
+      <div className="section">
+        <Router>
+          <Switch>
+            <Route
+              path='/'
+              exact
+              component={Login}
+            />
+            <Route
+              path='/home'
+              exact
+              component={Home}
+            />
+          </Switch>
+        </Router>
+      </div>
+    </div>)
+  }
 }
 
 export default App;
---
+++ src/pages/login.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import firebase from 'firebase'
+
+class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state =  {
+      loading: true
+    }
+  }
+
+  componentDidMount = async () => {
+    try {
+      let data = await firebase.auth().getRedirectResult()
+      if (data.credential) {
+        let {
+          history
+        } = this.props
+        history.push('/home')
+
+      } else {
+        this.setState({
+          loading: false
+        })
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  handleLoginWithSocialNetwork = async (service) => {
+    let provider
+    let stringService
+
+    if (service === 'facebook') {
+      stringService = 'FacebookAuthProvider'
+    } else if (service === 'google') {
+      stringService = 'GoogleAuthProvider'
+    }
+
+    provider = new firebase.auth[stringService]()
+    firebase.auth().signInWithRedirect(provider)
+  }
+
+  render() {
+
+    let {
+      loading
+    } = this.state
+
+    let content = <p className="cursive-font has-text-centered">Loading...</p>
+
+    if (!loading) {
+      content = (
+        <div className="buttons">
+          <button
+            onClick={() => this.handleLoginWithSocialNetwork('facebook')}
+            className="button is-fullwidth is-info"
+          >
+            Iniciar sesion con facebook
+          </button>
+
+          <button
+            onClick={() => this.handleLoginWithSocialNetwork('google')}
+            className="button is-fullwidth is-success"
+          >
+            Iniciar sesion con Google
+          </button>
+        </div>
+      )
+    }
+
+    return (<div className="columns">
+      <div className="column is-two-thirds">
+        <img src="/assets/preview.jpg" />
+      </div>
+      <div className="column">
+        <h1 className="title is-1 has-text-centered cursive-font main-title">
+          Insta Atom
+        </h1>
+        {
+          content
+        }
+      </div>
+    </div>)
+  }
+}
+
+export default Login;
